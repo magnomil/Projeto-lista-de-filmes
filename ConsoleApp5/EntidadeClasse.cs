299928c46d2dd17bcb43f4c8b7449f0d4d593c38
magnomil <67349729+magnomil@users.noreply.github.com>
2021-09-03 13:36:42 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp5/EntidadeClasse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public abstract class EntidadeClasse
+    {
+        // a classe vai ser abstratada porque todo que for herdar dessa classe vai pegar dela.
+        public int id { get; protected set; }
+
+    }
+}
---
+++ ConsoleApp5/Irepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public interface Irepositorio<T>
+    {
+        
+            List<T> Lista();
+            T RetornaPorId(int id);
+            void Insere(T entidade);
+            void Exclui(int id);
+            void Atualiza(int id, T entidade);
+            int ProximoId();
+        }
+    }
+
